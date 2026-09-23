8e01996ea6a73b9db6aa403cdaeb7cf929ca0de2
yScmw <banksetthaphong@gmail.com>
2026-09-23 15:39:11 +0700

    add comment

--- Hello.c
+++ Hello.c
@@ -11,6 +11,7 @@ int farewell(const char *name)
 printf("Goodbye, %s!\n", name);
 return 0;
 }
+// Print a check in message for the specified user
 int checkin(const char *name)
 {
     printf("%s are checked in\n, name");
